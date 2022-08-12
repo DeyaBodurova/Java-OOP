3d06eea394ccaf7419c7a273d324d7b4e3367b36
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-12 18:01:51 +0300

    wip

---
+++ src/main/java/glacialExpedition/repositories/StateRepository.java
@@ -0,0 +1,38 @@
+package glacialExpedition.repositories;
+
+import glacialExpedition.models.states.State;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class StateRepository implements Repository<State>{
+    private static Collection<State> states;
+
+    public StateRepository() {
+        states= new ArrayList<>();
+    }
+
+    @Override
+    public Collection<State> getCollection() {
+        return Collections.unmodifiableCollection(states);
+    }
+
+    @Override
+    public void add(State entity) {
+        states.add(entity);
+    }
+
+    @Override
+    public boolean remove(State entity) {
+        return states.remove(entity);
+    }
+
+    @Override
+    public State byName(String name) {
+        return states.stream()
+                .filter(s->s.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+}
