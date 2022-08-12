10a4f99a2738937529f676eb1a62dd79e8f82a77
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-12 17:01:55 +0300

    wip

---
+++ src/main/java/glacialExpedition/models/explorers/BaseExplorer.java
@@ -0,0 +1,11 @@
+package glacialExpedition.models.explorers;
+
+import glacialExpedition.models.suitcases.Suitcase;
+
+public abstract class BaseExplorer {
+    private static String name;
+    private static double energy;
+    private static Suitcase suitcase;
+
+
+}
